5a3558b8d7c49df22913b715a0caf2c3541ea10f
jesusfortuna <fortuna.codes@gmail.com>
2023-06-18 09:58:33 +0200

    Documenting code

---
+++ TaskBuddy/TaskBuddy/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  TaskBuddy
+//
+//  Created by Jesús  Fortuna on 18/6/23.
+//
+
+import Foundation
+
+//MARK: FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
